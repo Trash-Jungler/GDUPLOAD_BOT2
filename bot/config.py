62cd0b10f2376bcdf5609d87ca1de03755e261d5
Trash-Jungler <97131314+Trash-Jungler@users.noreply.github.com>
2022-01-24 19:01:39 -0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,12 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    BOT_TOKEN = "5215268049:AAF4t0chi4xTx4NytnjtxScZPsqDiq0RTzM"
+    APP_ID = "13088183"
+    API_HASH = "c3ccab059367d149ae30725123029e28"
+    DATABASE_URL = "postgres://ghbjseqffwxadt:8b3ed39db35fe64b96f17e01a6d7671ebfaece1565bcd07ab5a6cb4f8f0e28b5@ec2-54-208-139-247.compute-1.amazonaws.com:5432/dbu0dgvf1f8ghn"
+    SUDO_USERS = "1924845319" # Sepearted by space.
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "882823199948-fqtgkirlo3gnpnihrnh2k8q2qvpjn3r6.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "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"
     SUPPORT_CHAT_LINK = ""
 
 
